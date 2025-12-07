e6756819e6f1302185100f11efa5aa96377cc9e3
caleb <calebthair@outlook.com>
2025-12-07 01:50:13 +0000

    Tutorial waits for intro animation, added documentation

--- src/components/tutorial/tutorial.jsx
+++ src/components/tutorial/tutorial.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MOUSE_BTNS, TUTORIAL_STATE } from '../../constants';
+import { MOUSE_BTNS, TUTORIAL_STATE, EVENTS } from '../../constants';
 
 const progressEvents = [
   {
@@ -44,10 +44,20 @@ const progressAnimations = {
 export class Tutorial extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { progress: TUTORIAL_STATE.UNSTARTED };
+    this.state = {
+      progress: TUTORIAL_STATE.UNSTARTED,
+      hide: true,
+    };
     progressEvents.forEach((...args) => this.setupProgressEvent(...args));
+    document.addEventListener(EVENTS.INTRO_COMPLETE, () => this.setState({ hide: false }));
   }
 
+  /**
+   * Creates an event listener that attempts to progress the tutorial if a condition is met.
+   * @param eventName {string} the name of an event to trigger progression
+   * @param tutorialState {TUTORIAL_STATE} the state the progress should try to move to
+   * @param progressCondition {Function} a function that takes in an event and returns true if the progress should be moved forward
+   */
   setupProgressEvent({ eventName, tutorialState, progressCondition }) {
     const callback = (event) => {
       if (progressCondition(event) && this.updateProgress(tutorialState)) {
@@ -57,20 +67,24 @@ export class Tutorial extends React.Component {
     document.addEventListener(eventName, callback);
   }
 
+  /**
+   * Sets the progress to the value if it is the next step.
+   * @param tutorialState {TUTORIAL_STATE} the state of the tutorial to progress to.
+   * @return {boolean} true if the event listener that triggered it should be removed.
+   */
   updateProgress(tutorialState) {
     // If the new tutorial state is one step after the current
     if (tutorialState - 1 === this.state.progress) {
       this.setState({ progress: tutorialState });
-      return true;
     }
-    return false;
+    return tutorialState <= this.state.progress;
   }
 
   render() {
     const animation = progressAnimations[this.state.progress];
-    const hide = this.state.progress === TUTORIAL_STATE.PAN_COMPLETE;
+    const hide = this.state.hide || this.state.progress === TUTORIAL_STATE.PAN_COMPLETE;
     return (
-      <div className={`${hide ? 'hide-tutorial' : ''}`}>
+      <div className={`tutorial-icons ${hide ? 'hide-tutorial' : ''}`}>
         <div className="static-floor"></div>
         <div className={`reference-object ${animation}-animation`}></div>
         <div className={`tutorial-icon mouse-icon ${animation}-animation`}></div>
