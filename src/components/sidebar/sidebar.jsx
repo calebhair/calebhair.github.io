72790381371aa704d0dfaf8f535c51cceede591b
caleb <calebthair@outlook.com>
2025-12-13 17:04:03 +0000

    Changes to margin between desc and tags, refactored sidebar to be class based

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React from 'react';
 import {
   focusOnObjectIfValid, followTarget,
 } from '../../3d/focus';
@@ -8,80 +7,77 @@ import { moveToOverviewPos, animating } from '../../3d/cameraAnimation';
 import { EVENTS, PATHS } from '../../constants';
 import { SidebarEntry } from './sidebarEntry';
 
-/**
- * Sidebar component
- * @param {Array} planetJsonsToShow the list of planet JSON data
- * @return {JSX.Element}
- */
-export function Sidebar({ planetJsonsToShow }) {
-  const [visible, setVisible] = useState(false);
-  addEventListeners(setVisible);
+export class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+    this.addEventListeners();
+    this.planetEntries = this.makePlanetEntries(props.planetJsonsToShow);
+  }
 
-  const planetEntries = makePlanetEntries(planetJsonsToShow, setVisible);
-
-  const onCloseBtnClicked = () => {
-    setVisible(false);
-    document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
-  };
-
-  const onOverviewClick = () => {
-    moveToOverviewPos();
-    setVisible(false);
-    document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
-  };
-
-  return (
-    <div style={{ position: 'relative' }}>
-      <div className={`sidebar prevent-select ${visible ? 'show-sidebar' : ''}`}>
-        {/* Back button and re-centre */}
-        <SidebarEntry
-          imageUrl={window.innerWidth < 600 ? PATHS.UP_ARROW : PATHS.BACK_ARROW}
-          onClick={onCloseBtnClicked}
-        />
-        <SidebarEntry text="Re-centre" imageUrl={PATHS.QUASAR_ICON} onClick={onOverviewClick} classes="overview-entry" />
-        {planetEntries}
-      </div>
-    </div>
-  );
-}
-
-let eventListenersAdded = false;
-function addEventListeners(setVisible) {
-  if (eventListenersAdded) return;
-  eventListenersAdded = true;
-
-  document.addEventListener(EVENTS.SIDEBAR_OPENED, () => {
-    setVisible(true);
-  });
-
-  document.addEventListener(EVENTS.SIDEBAR_CLOSED, () => {
-    setVisible(false);
-  });
-
-  // Clicking away from the sidebar closes it
-  document.querySelector('#threejs-canvas')
-    .addEventListener('click', () => {
-      setVisible(false);
-
-      // Only set the nav btn state if a planet wasn't clicked; delay a little until followTarget would be set
-      setTimeout(() => {
-        if (!followTarget) document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
-      }, 100);
+  addEventListeners() {
+    document.addEventListener(EVENTS.SIDEBAR_OPENED, () => {
+      this.setState({ visible: true });
     });
-}
 
-function makePlanetEntries(planetJsons, setVisible) {
-  return planetJsons.map((planetJson, planetIndex) => (
-    <SidebarEntry
-      text={planetJson.name}
-      onClick={() => {
-        if (animating) return;
-        focusOnObjectIfValid(Planet.planets[planetIndex].model);
-        setVisible(false);
-      }}
-      classes="planet-entry"
-      imageUrl={planetJson.iconPath}
-      key={planetIndex}
-    />
-  ));
+    document.addEventListener(EVENTS.SIDEBAR_CLOSED, () => {
+      this.setState({ visible: false });
+    });
+
+    // Clicking away from the sidebar closes it
+    document.querySelector('#threejs-canvas')
+      .addEventListener('click', () => {
+        this.setState({ visible: false });
+
+        // Only set the nav btn state if a planet wasn't clicked; delay a little until followTarget would be set
+        setTimeout(() => {
+          if (!followTarget) document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
+        }, 100);
+      });
+  }
+
+  onCloseBtnClicked() {
+    this.setState({ visible: false });
+    document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
+  }
+
+  onOverviewClick() {
+    moveToOverviewPos();
+    this.setState({ visible: false });
+    document.dispatchEvent(new Event(EVENTS.SET_NAV_BTN_DEFAULT));
+  };
+
+  makePlanetEntries(planetJsons, setVisible) {
+    return planetJsons.map((planetJson, planetIndex) => (
+      <SidebarEntry
+        text={planetJson.name}
+        onClick={() => {
+          if (animating) return;
+          focusOnObjectIfValid(Planet.planets[planetIndex].model);
+          setVisible(false);
+        }}
+        classes="planet-entry"
+        imageUrl={planetJson.iconPath}
+        key={planetIndex}
+      />
+    ));
+  }
+
+  render() {
+    return (
+      <div style={{ position: 'relative' }}>
+        <div className={`sidebar prevent-select ${this.state.visible ? 'show-sidebar' : ''}`}>
+          {/* Back button and re-centre */}
+          <SidebarEntry
+            imageUrl={window.innerWidth < 600 ? PATHS.UP_ARROW : PATHS.BACK_ARROW}
+            onClick={() => this.onCloseBtnClicked()}
+          />
+          <SidebarEntry text="Re-centre" imageUrl={PATHS.QUASAR_ICON} onClick={() => this.onOverviewClick()} classes="overview-entry" />
+          {this.planetEntries}
+        </div>
+      </div>
+    );
+  }
 }
