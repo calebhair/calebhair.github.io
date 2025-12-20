071ab608fbea1f95e2c0fc0892ef7fd4c7b531d1
caleb <calebthair@outlook.com>
2025-12-20 02:01:25 +0000

    Added progress indicator to tutorial, added transition between tutorial steps

--- src/components/tutorial/tutorial.jsx
+++ src/components/tutorial/tutorial.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { MOUSE_BTNS, TUTORIAL_STATE, EVENTS, COOKIES } from '../../constants';
+import { MOUSE_BTNS, TUTORIAL_STATE, EVENTS } from '../../constants';
+
+const PROGRESS_INDICATOR_TIME = 1000; // ms to show green progress indicator
+const PROGRESS_HIDDEN_TIME = 500; // ms to hide tutorial before showing next tutorial step
 
 const progressEvents = [
   {
-    eventName: 'mousedown',
+    eventName: 'mouseup',
     tutorialState: TUTORIAL_STATE.ROTATION_COMPLETE,
     progressCondition: event => event.button === MOUSE_BTNS.LEFT,
   },
@@ -23,7 +26,7 @@ const progressEvents = [
     progressCondition: event => event.touches.length === 2,
   },
   {
-    eventName: 'mousedown',
+    eventName: 'mouseup',
     tutorialState: TUTORIAL_STATE.PAN_COMPLETE,
     progressCondition: event => event.button === MOUSE_BTNS.RIGHT,
   },
@@ -34,13 +37,13 @@ const progressEvents = [
   },
 ];
 
+// This maps tutorial states to the corresponding animation class names
 const progressAnimations = {
   [TUTORIAL_STATE.UNSTARTED]: 'rotate',
   [TUTORIAL_STATE.ROTATION_COMPLETE]: 'zoom',
   [TUTORIAL_STATE.ZOOM_COMPLETE]: 'pan',
   [TUTORIAL_STATE.PAN_COMPLETE]: 'hide',
 };
-
 const COMPLETE_STATE = TUTORIAL_STATE.PAN_COMPLETE;
 
 export class Tutorial extends React.Component {
@@ -48,17 +51,13 @@ export class Tutorial extends React.Component {
     super(props);
     this.state = {
       progress: TUTORIAL_STATE.UNSTARTED,
-      hide: true,
+      indicateProgressMade: false,
+      hidden: true,
     };
-
-    if (document.cookie.includes('tutorial_complete=true')) {
-      this.state.prototype = COMPLETE_STATE;
-      return;
-    }
 
     document.addEventListener(EVENTS.INTRO_COMPLETE, () => {
       progressEvents.forEach((...args) => this.setupProgressEvent(...args));
-      this.setState({ hide: false });
+      this.setState({ hidden: false });
     });
     document.addEventListener('touchstart', () => this.touchDown = true);
     document.addEventListener('touchend', () => this.touchDown = false);
@@ -72,7 +71,7 @@ export class Tutorial extends React.Component {
    */
   setupProgressEvent({ eventName, tutorialState, progressCondition }) {
     const callback = (event) => {
-      if (progressCondition(event) && this.updateProgress(tutorialState)) {
+      if (progressCondition(event) && this.tryUpdateProgress(tutorialState)) {
         document.removeEventListener(eventName, callback);
       }
     };
@@ -84,19 +83,32 @@ export class Tutorial extends React.Component {
    * @param tutorialState {TUTORIAL_STATE} the state of the tutorial to progress to.
    * @return {boolean} true if the event listener that triggered it should be removed.
    */
-  updateProgress(tutorialState) {
-    // If the new tutorial state is one step after the current
-    if (tutorialState - 1 === this.state.progress) {
-      // If touch is down, wait until it is no longer down, via timeout and recursion
-      if (this.touchDown) {
-        setTimeout(() => this.updateProgress(tutorialState), 500);
-      }
-      else {
-        this.setState({ progress: tutorialState });
-        if (tutorialState === COMPLETE_STATE) this.onComplete();
-      }
+  tryUpdateProgress(tutorialState) {
+    // If touch is down, wait until it is no longer down, via timeout and recursion.
+    // This prevents accidentally skipping tutorial steps.
+    if (this.touchDown) {
+      setTimeout(() => this.tryUpdateProgress(tutorialState), 500);
+      return false;
     }
+
+    // If the new tutorial state is one step after the current
+    if (tutorialState - 1 === this.state.progress) this.onProgress(tutorialState);
     return tutorialState <= this.state.progress;
+  }
+
+  onProgress(tutorialState) {
+    this.setState({ indicateProgressMade: true });
+
+    // Triggers hiding tutorial
+    setTimeout(() => {
+      this.setState({ hidden: true });
+    }, PROGRESS_INDICATOR_TIME);
+
+    // Triggers showing next tutorial step
+    setTimeout(() => {
+      this.setState({ progress: tutorialState, indicateProgressMade: false, hidden: false });
+      if (tutorialState === COMPLETE_STATE) this.onComplete();
+    }, PROGRESS_INDICATOR_TIME + PROGRESS_HIDDEN_TIME);
   }
 
   onComplete() {
@@ -104,17 +116,19 @@ export class Tutorial extends React.Component {
   }
 
   render() {
-    const animation = progressAnimations[this.state.progress];
-    const hide = this.state.hide || this.state.progress === COMPLETE_STATE;
+    const { progress, indicateProgressMade, hidden } = this.state;
+    const animation = progressAnimations[progress];
+    const hide = hidden || progress === COMPLETE_STATE;
+    const progressClass = indicateProgressMade ? 'progress-made' : '';
     return (
       <div className={`tutorial ${hide ? 'hide-tutorial' : ''}`}>
         <div className="reference-environment">
           <div className="static-floor"></div>
-          <div className={`reference-object ${animation}-animation`}></div>
+          <div className={`reference-object ${animation}-animation ${progressClass}`}></div>
         </div>
         <div className="tutorial-peripheral-icon-container">
-          <div className={`tutorial-peripheral-icon mouse-icon ${animation}-animation`}></div>
-          <div className={`tutorial-peripheral-icon touch-icon ${animation}-animation`}></div>
+          <div className={`tutorial-peripheral-icon mouse-icon ${animation}-animation ${progressClass}`}></div>
+          <div className={`tutorial-peripheral-icon touch-icon ${animation}-animation ${progressClass}`}></div>
         </div>
       </div>
     );
